6cb5f6d4110ab3839c3a4745c9af815ab7ded806
Dovran Miliyev <dovran.hec@gmail.com>
2020-10-07 16:01:00 +0300

    Completed the calculation of dog years

---
+++ script.js
@@ -0,0 +1,12 @@
+var myAge = prompt("age") ; //My age in numbers
+let earlyYears = 2; // variable set by us, will change later
+earlyYears = (earlyYears * 10.5);
+let laterYears = myAge - 2; //After 2 yo counts multilply differently
+laterYears*=4
+//console.log(earlyYears, laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+const myName = prompt ("Whats your name?")
+alert ('My name is ' + myName.toLowerCase() + '.I am ' + myAge + 'years old in human years which is ' + myAgeInDogYears + 'years old in dog years.')
+
+console.log('My name is ' + myName.toLowerCase() + '.I am ' + myAge + 'years old in human years which is ' + myAgeInDogYears + 'years old in dog years.')
+
